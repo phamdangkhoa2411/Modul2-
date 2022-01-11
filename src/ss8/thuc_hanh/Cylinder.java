5301a871f98ff27edc987bb66da11815f855f8c3
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-01-11 13:47:51 +0700

    push

---
+++ src/ss8/thuc_hanh/Cylinder.java
@@ -0,0 +1,19 @@
+package ss8.thuc_hanh;
+
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getRadius(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
+    }
+
+    private static int getRadius(int radius) {
+        return radius;
+    }
+
+    private static double getBaseArea(int radius) {
+
+        return Math.PI * radius * radius;
+    }
+}
