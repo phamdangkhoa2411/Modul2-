5442cc67c2c575dd9ba5070b634c873ccf36fd9e
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-01-13 22:29:07 +0700

    push

--- src/ss11_stack_and_queue/bai_tap/ReverseElementIntergerArray.java
+++ src/ss11_stack_and_queue/bai_tap/ReverseElementIntergerArray.java
@@ -1,2 +1,30 @@
-package ss11_stack_and_queue.bai_tap;public class ReverseElementIntergerArray {
+package ss11_stack_and_queue.bai_tap;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseElementIntergerArray {
+    public static void main(String[] args) {
+
+//Đảo ngược phần tử trong mảng số nguyên sử dụng Stack
+        Stack<Integer> myStack = new Stack<>();
+        Integer[] integer = new Integer[3];
+        integer[0] = myStack.push(1);
+        integer[1] = myStack.push(2);
+        integer[2] = myStack.push(5);
+
+        for (Integer integers :integer){
+            System.out.print(integers+" ");
+        }
+        System.out.println();
+        integer[0] = myStack.pop();
+        integer[2] = myStack.push(1);
+
+        for (Integer integers :integer){
+            System.out.print(integers+" ");
+        }
+    }
+
 }
+
+
