5e00cc26a12a90b32587694d57f8a27ab0a1ad8f
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-01-18 13:58:01 +0700

    push

--- src/ss12_java_collection_framework/bai_tap/service/impl/ProductImpl.java
+++ src/ss12_java_collection_framework/bai_tap/service/impl/ProductImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ProductImpl implements ProductService {
+
     static List<Product> productList = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
 
@@ -20,10 +21,8 @@ public class ProductImpl implements ProductService {
         String name = scanner.nextLine();
         System.out.println("Mời bạn nhập giá sản phẩm cần thêm:");
         String gia = scanner.nextLine();
-        Product product =new Product(id,name,gia);
+        Product product = new Product(id, name, gia);
         productList.add(product);
-
-
 
 
     }
@@ -31,46 +30,65 @@ public class ProductImpl implements ProductService {
     @Override
     public void edit() {
         System.out.print("Mời bạn nhập id sản phẩm cần chỉnh sửa: ");
-        int idToEdit = Integer.parseInt(scanner.nextLine());
+        String idEdit = scanner.nextLine();
         for (int i = 0; i < productList.size(); i++) {
-            if ( idToEdit== productList.get(i).getId() ){
+            if (productList.get(i).getId().equals(idEdit)) {
                 System.out.println("Mời bạn nhập tên cần sửa: ");
-                String newName=scanner.nextLine();
+                String newName = scanner.nextLine();
                 System.out.println("Mời bạn nhập giá cần sửa: ");
-                String newGia=scanner.nextLine();
-
+                String newGia = scanner.nextLine();
                 productList.get(i).setName(newName);
                 productList.get(i).setGia(newGia);
 
             }
         }
-
     }
 
+        @Override
+        public void delete () {
+            System.out.println("nhập id sản phẩm muốn xóa");
+            String id = scanner.nextLine();
+            for (int i = 0; i <productList.size() ; i++) {
+                if (productList.get(i).getId().equals(id));
+                productList.remove(i);
 
+            }
 
-    @Override
-    public void delete() {
+        }
 
+        @Override
+        public void display () {
+            for (Product product:productList) {
+                System.out.println(product);
+
+            }
+
+        }
+
+        @Override
+        public void search () {
+            System.out.println("nhập tên sản phẩm cần tìm : ");
+             String name = scanner.nextLine();
+             Boolean check = false;
+            for (Product product:productList) {
+                if (product.getName().equals(name)){
+                    System.out.println("sản phẩm tìm kiếm là :"+product);
+                    check = true;
+                }
+            }
+                if (!check){
+                    System.out.println("không tìm thấy sản phẩm");
+                }
+        }
+        
+
+        @Override
+        public void increase () {
+
+        }
+
+        @Override
+        public void decrease () {
+
+        }
     }
-
-    @Override
-    public void display() {
-
-    }
-
-    @Override
-    public void search() {
-
-    }
-
-    @Override
-    public void increase() {
-
-    }
-
-    @Override
-    public void decrease() {
-
-    }
-}
