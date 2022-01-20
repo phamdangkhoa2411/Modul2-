116d1c64474a283cd643626f914d1d0761ec1a59
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-01-20 13:06:04 +0700

    push

---
+++ src/ss15_debug_and_exception/thuc_hanh/CalculationExample.java
@@ -0,0 +1,37 @@
+package ss15_debug_and_exception.thuc_hanh;
+
+import java.util.Scanner;
+
+public class CalculationExample {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println(" moi nhap phan tu a");
+        int a= scanner.nextInt();
+        System.out.println(" moi nhap phantu b");
+        int b= scanner.nextInt();
+        CalculationExample calculationExample= new CalculationExample();
+        calculationExample.calcula(a,b);
+
+    }
+
+
+    public  void calcula(int a, int b){
+
+        try{
+            int i= a+b;
+            int k= a-b;
+            int l= a*b;
+            int m= a/b;
+
+            System.out.println(" tong a+b = "+ i);
+            System.out.println(" tong a-b = "+ k);
+            System.out.println(" tong a*b = "+ l);
+            System.out.println(" tong a/b = "+ m);
+
+        }catch(Exception e){
+            System.out.println(" kiểu dũ liêu bạn nhập vào bị sai ");
+        }
+
+
+    }
+}
