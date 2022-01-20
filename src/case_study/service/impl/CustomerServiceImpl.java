b8958458fdcad0fafa9b9851f360a7245bf59e15
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-01-20 13:06:22 +0700

    push

--- src/case_study/service/impl/CustomerServiceImpl.java
+++ src/case_study/service/impl/CustomerServiceImpl.java
@@ -1,20 +1,131 @@
 package case_study.service.impl;
 
+import case_study.model.Person.Customer;
+import case_study.model.Person.Person;
 import case_study.service.IPersonService;
 
-public class CustomerServiceImpl implements IPersonService {
-    @Override
-    public void edit() {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    }
+public class CustomerServiceImpl implements IPersonService {
+    static List<Customer> customerList = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
 
     @Override
     public void addNew() {
+
+            System.out.println("nhập id");
+            String id = scanner.nextLine();
+            System.out.println("nhập fullName");
+            String fullName = scanner.nextLine();
+            System.out.println("nhập dayOfBirth");
+            String dayOfBirth = scanner.nextLine();
+            System.out.println("nhập sex ");
+            String sex = scanner.nextLine();
+            System.out.println("nhập  identityCardNo ");
+            String identityCardNo = scanner.nextLine();
+            System.out.println("nhập phoneNo");
+            String phoneNo = scanner.nextLine();
+            System.out.println("nhập email ");
+            String email = scanner.nextLine();
+            System.out.println("nhập customerType");
+            String customerType = scanner.nextLine();
+            System.out.println("nhập  address");
+            String address = scanner.nextLine();
+            Customer customer = new Customer(id, fullName, dayOfBirth, sex, identityCardNo, phoneNo, email,customerType,address);
+            customerList.add(customer);
+
 
     }
 
     @Override
     public void display() {
+        for (Customer customer:customerList) {
+            System.out.println(customer);
+
+        }
+
+    }
+    @Override
+    public void edit() {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Nhập id cần sửa ");
+        String idEdit = scanner.nextLine();
+        boolean check = false;
+        for (Person customer : customerList ){
+            System.out.println(customer.getId());
+            if (customer.getId() == idEdit) {
+                check = true;
+                System.out.println("Chọn phần cần sửa");
+                System.out.println("2. name");
+                System.out.println("3. dateOfBirth");
+                System.out.println("4. sex");
+                System.out.println("5. identityCardNo ");
+                System.out.println("6. numberPhone");
+                System.out.println("7. gmail");
+                System.out.println("8.  customerType");
+                System.out.println("9. address");
+
+                int choice = scanner.nextInt();
+                scanner.skip("\\R");
+                switch (choice){
+                    case 2: {
+                        System.out.println("sua ten");
+                        String nameEdit = scanner.nextLine();
+                        customer.setFullName(nameEdit);
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("sua ngay sinh");
+                        String DayOfBirthEdit = scanner.nextLine();
+                        customer.setDayOfBirth(DayOfBirthEdit);
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("sua gioi tinh");
+                        String sexEdit = scanner.nextLine();
+                        customer.setSex(sexEdit);
+                        break;
+                    }
+                    case 5: {
+                        System.out.println("sua identityCardNo  ");
+                        String identityCardNoEdit  = scanner.nextLine();
+                        customer.setIdentityCardNo(identityCardNoEdit);
+                        break;
+                    }
+                    case 6: {
+                        System.out.println("sua phoneNo");
+                        String phoneNoEdit = scanner.nextLine();
+                        customer.setPhoneNo(phoneNoEdit);
+                        break;
+                    }
+                    case 7: {
+                        System.out.println("sua email");
+                        String emailEdit  = scanner.nextLine();
+                        customer.setEmail(emailEdit);
+                        break;
+                    }
+                    case 8: {
+                        System.out.println("sua  customerType");
+                        String  customerTypeEdit = scanner.nextLine();
+                        ((Customer) customer).setCustomerType( customerTypeEdit);
+                        break;
+                    }
+                    case 9: {
+                        System.out.println("sua  address");
+                        String  addressEdit = scanner.nextLine();
+                        ((Customer) customer).setAddress( addressEdit);
+                        break;
+                    }
+
+                }
+            } else if (!check) {
+                System.out.println("ID not found!");
+
+            }
+        }
 
     }
 
--- src/case_study/service/impl/EmployeeServiceImpl.java
+++ src/case_study/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package case_study.service.impl;
 
 import case_study.model.Person.Employee;
+import case_study.model.Person.Person;
 import case_study.service.IPersonService;
 
 import java.util.ArrayList;
@@ -11,11 +12,6 @@ public class EmployeeServiceImpl implements IPersonService {
     static List<Employee> employeeList = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
 
-    @Override
-    public void edit() {
-        System.out.println("Nhập id ");
-
-    }
 
     @Override
     public void addNew() {
@@ -53,4 +49,93 @@ public class EmployeeServiceImpl implements IPersonService {
         }
 
     }
-}
+    @Override
+    public void edit() {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Nhập id cần sửa ");
+        String idEdit = scanner.nextLine();
+        boolean check = false;
+        for (Person employee: employeeList){
+            System.out.println(employee.getId());
+            if (employee.getId() == idEdit) {
+                check = true;
+                System.out.println("Chọn phần cần sửa");
+                System.out.println("2. name");
+                System.out.println("3. dateOfBirth");
+                System.out.println("4. sex");
+                System.out.println("5. identityCardNo ");
+                System.out.println("6. numberPhone");
+                System.out.println("7. gmail");
+                System.out.println("8. level");
+                System.out.println("9. position");
+                System.out.println("10. salary");
+                int choice = scanner.nextInt();
+                scanner.skip("\\R");
+                switch (choice){
+                    case 2: {
+                        System.out.println("sua ten");
+                        String nameEdit = scanner.nextLine();
+                        employee.setFullName(nameEdit);
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("sua ngay sinh");
+                        String DayOfBirthEdit = scanner.nextLine();
+                        employee.setDayOfBirth(DayOfBirthEdit);
+                        break;
+                    }
+                    case 4: {
+                        System.out.println("sua gioi tinh");
+                        String sexEdit = scanner.nextLine();
+                        employee.setSex(sexEdit);
+                        break;
+                    }
+                    case 5: {
+                        System.out.println("sua identityCardNo  ");
+                        String identityCardNoEdit  = scanner.nextLine();
+                        employee.setIdentityCardNo(identityCardNoEdit);
+                        break;
+                    }
+                    case 6: {
+                        System.out.println("sua phoneNo");
+                        String phoneNoEdit = scanner.nextLine();
+                        employee.setPhoneNo(phoneNoEdit);
+                        break;
+                    }
+                    case 7: {
+                        System.out.println("sua email");
+                        String emailEdit  = scanner.nextLine();
+                        employee.setEmail(emailEdit);
+                        break;
+                    }
+                    case 8: {
+                        System.out.println("sua education");
+                        String educationEdit = scanner.nextLine();
+                        ((Employee) employee).setEducation(educationEdit);
+                        break;
+                    }
+                    case 9: {
+                        System.out.println("sua vi tri");
+                        String PositionEdit = scanner.nextLine();
+                        ((Employee) employee).setPoisition(PositionEdit);
+                        break;
+                    }
+                    case 10: {
+                        System.out.println("sua luong");
+                        int SalaryEdit = scanner.nextInt();
+                        ((Employee) employee).setSalary(SalaryEdit);
+                        break;
+                    }
+
+                }
+            } else if (!check) {
+                System.out.println("ID not found!");
+
+            }
+                }
+            }
+
+        }
+
+
+
